c2d6610a232fd0a393e63accf78e6187583d74d2
AMMAR <141614147+AmmarTheDeveloper@users.noreply.github.com>
2024-05-09 18:13:55 +0530

    saveUninitalized : false to true

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use( cors( {
 app.use( session( {
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     store: MongoStore.create( {
         client: mongoose.connection.getClient()
     } ),
