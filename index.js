6dd3cc58c66400d9d9f965a0b2e14b6cd2841fb7
AMMAR <141614147+AmmarTheDeveloper@users.noreply.github.com>
2024-05-10 18:02:51 +0530

    updated session configuration because the previous configuration also not working means i am not able to get the session value after setting it

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const MongoStore = require( 'connect-mongo' );
 require( 'dotenv' ).config();
 const redisService = require( './services/redis' )
 
-
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect( process.env.DB_CONNECTION )
@@ -23,6 +22,7 @@ app.use( cors( {
     credentials: true,
     methods: [ "GET", "POST", "PUT", "DELETE" ]
 } ) );
+app.set( "trust proxy", 1 ); // trust first proxy
 app.use( session( {
     secret: process.env.SESSION_SECRET,
     resave: false,
@@ -33,7 +33,7 @@ app.use( session( {
         client: mongoose.connection.getClient()
     } ),
     cookie: {
-        // secure: true,
+        secure: true,
         httpOnly: true,
         sameSite: 'none'
     }
